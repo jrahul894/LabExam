231a2991b3eab10910b59155b8a63cf96caa7ebf
jrahul894 <rjain17609@gmail.com>
2023-07-25 09:16:10 +0530

    MaximumOne is first time modified

--- MaximumOne.java
+++ MaximumOne.java
@@ -1,5 +1,24 @@
-FROM openjdk
-WORKDIR /app
-COPY ./MaximumOne.java .
-RUN javac MaximumOne.java
-CMD ["java", "MaximumOne"]
+import java.util.Scanner;
+
+public class MaximumOne {
+
+	public static int findLargest(int a,int b,int c)
+	{
+	   	
+		
+		return a>b?(a>c)?a:c:(b>c)?b:c;
+		
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter three numbers");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		int sum = num1 + num2 + num3;
+		System.out.println(sum);
+        System.out.println(findLargest(num1,num2,num3));
+	}
+
+}
